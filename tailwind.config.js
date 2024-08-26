8919ead81225e56cd3bcfe6b70d77b54a40d9e1f
unknown <106021129+dzakyadlh@users.noreply.github.com>
2024-08-26 23:56:18 +0700

    initial push

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    // colors: {
+    //   transparent: "transparent",
+    //   current: "currentColor",
+    //   "bg-primary": "#010003",
+    // },
+  },
+  plugins: [],
+};
