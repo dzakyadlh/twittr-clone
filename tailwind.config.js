363d3e1f2fb615e67471b721fdfbb7bc5823ea61
unknown <106021129+dzakyadlh@users.noreply.github.com>
2024-08-28 09:15:40 +0700

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,22 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
-    // colors: {
-    //   transparent: "transparent",
-    //   current: "currentColor",
-    //   "bg-primary": "#010003",
-    // },
+    extend: {
+      colors: {
+        "primary-bg": "#010003",
+        "secondary-bg": "#121316",
+        "border-gray": "#1E1E1E",
+        "subtitle-text": "#5E6368",
+        "twittr-blue": "#1880E8",
+        "likes-pink": "#F91880",
+      },
+      fontSize: {
+        "subtitle-size": "14px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
